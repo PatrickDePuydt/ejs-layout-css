ed78f37788b58d40bb62a45e75972e3f145a9a20
PatrickDePuydt <prdepuydt@gmail.com>
2021-01-11 14:20:17 -0700

    master: Add event listener

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,11 @@ let colors = [];
 let animals = document.getElementsByClassName('animal');;
 
 const colorGen = () => { 
-  for(let i = 0; i <3; i++) {
-    colors[i] = Math.floor(Math.random() * 255);
-  }
+  // for(let i = 0; i <3; i++) {
+  //   colors[i] = Math.floor(Math.random() * 255);
+  // }
 
+  console.log(`Working`);
 };
+
+document.getElementById('button').addEventListener('click', colorGen);
